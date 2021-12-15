0dc7595120ed5cebd331d0be582642b50bb44cd4
danny-1001 <shazam1258@gmail.com>
2021-12-15 13:25:30 -0500

    finished bootstrap layout, now I'll go back to customize font, colors, etc, with Sass

---
+++ app.js
@@ -0,0 +1,8 @@
+mapboxgl.accessToken =
+  "pk.eyJ1IjoiZGFubnkwMTEwIiwiYSI6ImNreDd1ODZodjEzZWsydnF1bGZmZTdhZDIifQ.qzBjesiX3itYVGytWgJV3g";
+var map = new mapboxgl.Map({
+  container: "map",
+  style: "mapbox://styles/mapbox/streets-v11",
+  center: [40.7831, 73.9712],
+  zoom: 10,
+});
